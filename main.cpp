9dc69f634a0809f8d8443e57591072c98626b8f6
@HermesonAlessandro <105324312+HermesonAlessandro@users.noreply.github.com>
2024-04-27 00:19:56 -0300

    Alteraçoes basicas
    Alteraçoes basicas

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
-	printf("ola mundo");
+
+int main() {
+	LC_ALL, "";
+	printf("Ola mundo!"); 
 	return 0;
 }
